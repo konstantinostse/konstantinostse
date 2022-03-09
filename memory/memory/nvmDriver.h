5b90ec68aec81d5e6d5a140ed61df0763d4f6266
Polymitis <petros@polymitis.com>
2022-03-09 14:32:14 +0000

    Update nvmDriver.h

--- memory/memory/nvmDriver.h
+++ memory/memory/nvmDriver.h
@@ -14,12 +14,21 @@
 
 #include <stdio.h>
 
+/* PF: Comment: 
+ 1. Why use of int instead of uint32_t for the address field ? Unless you support relative addressing please use unsigned types.
+ 2. Please put in the function description the address lenght and maximum memory size supported by the read function.
+ 3. The doxygen documentation should denote the direction of parameters, since C doens't offer any protection in case of pointers.
+ 4. Please standardize error reporting by creating nvmDriverError.h having all the error codes, even if its just one error for failure.
+ 
+ Comments apply on all functions.
+*/
+
 /** @brief Read N (byteSize) Bytes from the NVM 
  *         in the address nvmAddress and write them in the Buffer.
- *  @param buffer Array or Struct to write the bytes from the NVM.
- *  @param nvmAddress The address in NVM from which the buffer will read the N (byteSize) Bytes. 
- *  @param byteSize The size of bytes which they will be read from the NVM. 
- *  @return Read status. 0 for Success and -1 for failure.
+ *  @param[out]   buffer      Pointer to an array or Struct to write the bytes from the NVM.
+ *  @param[in]    nvmAddress  The address in NVM from which the buffer will read the N (byteSize) Bytes. 
+ *  @param[int]   byteSize    The size of bytes which they will be read from the NVM. 
+ *  @return                   Read status. 0 for Success and -1 for failure.
  */
 int readNvm( void* const buffer, int nvmAddress, int byteSize);
 
